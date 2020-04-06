d5629ca5b9d44d4e7dcfdf65c7ac4f9ee1e65124
liujingwen <liujingwen718@gmail.com>
2020-04-06 19:45:26 +0800

    【第2题】两数相加

--- src/2.addTwoNumbers.c
+++ src/2.addTwoNumbers.c
@@ -1,11 +1,6 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+
 /*
+2. 两数相加
 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
 
 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
@@ -20,6 +15,13 @@
 
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     int arr[1000]={0};  //临时存储 
