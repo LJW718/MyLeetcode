a5a894a6193f335b23c5422b08096d6e487dfaab
liujingwen <liujingwen718@gmail.com>
2020-06-25 15:24:05 +0800

    新增CMake工程

---
+++ cmake-build-debug/CMakeFiles/MyLeetcode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyLeetcode.dir/main.c.obj"
+  "MyLeetcode.exe"
+  "MyLeetcode.exe.manifest"
+  "MyLeetcode.pdb"
+  "libMyLeetcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyLeetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
